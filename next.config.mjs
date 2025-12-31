f7cebca51f8db8335e3fd4b6aa00c1b8f67392af
KalarianAthecila <FabianSchaefer2022@outlook.de>
2025-12-31 17:13:00 +0100

    Add social links

--- next.config.mjs
+++ next.config.mjs
@@ -18,6 +18,31 @@ const nextConfig = {
         destination: 'https://github.com/Soncresity-Industries/snh/releases/latest/download/mods.zip',
         permanent: true,
       },
+      {
+        source: '/patreon',
+        destination: 'https://www.patreon.com/c/SoncresityIndustries',
+        permanent: true,
+      },
+      {
+        source: '/discord',
+        destination: 'https://discord.gg/uqbQvAHHve',
+        permanent: true,
+      },
+      {
+        source: '/curseforge',
+        destination: 'https://www.curseforge.com/members/soncresityindustries/projects',
+        permanent: true,
+      },
+      {
+        source: '/modrinth',
+        destination: 'https://modrinth.com/user/SoncresityIndustries',
+        permanent: true,
+      },
+      {
+        source: '/youtube',
+        destination: 'https://www.youtube.com/@SoncresityIndustries',
+        permanent: true,
+      },
     ]
   },
 }
