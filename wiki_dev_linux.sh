c296914896f2a2e431df6cd91ab59b84817d2635
SkyKing_PX <skyking_px@soncresity.dev>
2026-03-01 17:47:16 +0000

    Added linux dev scripts

---
+++ wiki_dev_linux.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+SCRIPT_DIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
+cd "$SCRIPT_DIR/wiki" || exit 1
+
+if [ ! -d "node_modules" ]; then
+    echo "Installing npm dependencies..."
+    npm i --force
+fi
+
+npm run dev
